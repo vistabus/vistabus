f9614172fca409d761492a67efb23fbe2849e16a
ZacharyLaw <law.zachary@gmail.com>
2022-08-29 14:48:12 +0800

    Updates

---
+++ precache-manifest.52405f469d6f4a47e47d452fe15272d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98074d05807842c85248",
+    "url": "/static/css/main.41ddb1f1.chunk.css"
+  },
+  {
+    "revision": "98074d05807842c85248",
+    "url": "/static/js/main.1b0ad612.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "46ed667e0b2f208d60d2",
+    "url": "/static/css/2.54a74aff.chunk.css"
+  },
+  {
+    "revision": "46ed667e0b2f208d60d2",
+    "url": "/static/js/2.8daedb49.chunk.js"
+  },
+  {
+    "revision": "5d9b7ed457106b757ec872c535d35e85",
+    "url": "/index.html"
+  }
+];
