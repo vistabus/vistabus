9d56789041040c12b580a65523aac4aeda3497d1
ZacharyLaw <law.zachary@gmail.com>
2022-09-03 21:06:18 +0800

    about to pull

--- src/component/next.js
+++ src/component/next.js
@@ -1,7 +1,10 @@
 import $ from 'jquery'
 import React,{ useState } from 'react';
+import moment from 'moment';
 
 export default function Next(){
+  var dia1,ma1,uni1
+
 const [nextdia,setnextdia]=useState();
 const [nextma,setnextma]=useState();
 const [nextuni,setnextuni]=useState();
@@ -10,6 +13,13 @@ const [dia,setdia]=useState();
 const [ma,setma]=useState();
 const [uni,setuni]=useState();
 
+const [dia2,setdia2]=useState();
+const [ma2,setma2]=useState();
+const [uni2,setuni2]=useState();
+
+const [nextdia2,setnextdia2]=useState();
+const [nextma2,setnextma2]=useState();
+const [nextuni2,setnextuni2]=useState();
 
 function addMinutes(time, minsToAdd) {
     function D(J){ return (J<10? '0':'') + J;};
@@ -23,10 +33,21 @@ setInterval(function(){
     setma(window.maonshan)
     setuni(window.university)
 
-    setnextdia(addMinutes(window.nextdiamondhill+':00','15'))
-    setnextma(addMinutes(window.nextmaonshan+':00','5'))
-    setnextuni(addMinutes(window.nextuniversity+':00','10'))
+    dia1=addMinutes(window.nextdiamondhill+':00','15')
+    ma1=addMinutes(window.nextmaonshan+':00','5')
+    uni1=addMinutes(window.nextuniversity+':00','10')
 
+    setnextdia(dia1)
+    setnextma(ma1)
+    setnextuni(uni1)
+
+    setdia2(moment(window.diamondhill,'h:m').fromNow())
+    setma2(moment(window.maonshan,'h:m').fromNow())
+    setuni2(moment(window.university,'h:m').fromNow())
+
+    setnextdia2(moment(dia1,'h:m').fromNow())
+    setnextma2(moment(ma1,'h:m').fromNow())
+    setnextuni2(moment(uni1,'h:m').fromNow())
 
 
     
@@ -35,21 +56,30 @@ setInterval(function(){
 
 return (<div id='nextdiv'>
 
-<aside id='dia'><h2>DIAMOND HILL</h2>
-<a>{dia}</a>DEP.<span>{nextdia}</span>
-<a>{dia}</a>DEP.<span>{nextdia}</span>
-
-<h3>Vista Paradiso</h3></aside>
-
-
-<aside id='uni'><h2>UNIVERSITY</h2>
-<a>{uni}</a>DEP.<span>{nextuni}</span>
-<h3>Vista Paradiso</h3></aside>
+<aside id='dia'>
+<table><tbody><tr>
+  <td><h2>DIAMOND HILL</h2><br></br>{dia}<br></br>{dia2}</td>
+  <td>DEP.</td>
+  <td>{nextdia}<br></br>{nextdia2}<br></br><h3>VISTA PARADISO</h3></td>
+  </tr></tbody></table>
+</aside>
 
 
-<aside id='ma'><h2>MA ON SHAN</h2>
-<a>{ma}</a>DEP.<span>{nextma}</span>
-<h3>Vista Paradiso</h3></aside>
+<aside id='uni'>
+<table><tbody><tr>
+  <td><h2>UNIVERSITY</h2><br></br>{uni}<br></br>{uni2}</td>
+  <td>DEP.</td>
+  <td>{nextuni}<br></br>{nextuni2}<br></br><h3>VISTA PARADISO</h3></td>
+  </tr></tbody></table>
+</aside>
+
+<aside id='ma'>
+<table><tbody><tr>
+  <td><h2>MA ON SHAN</h2><br></br>{ma}<br></br>{ma2}</td>
+  <td>DEP.</td>
+  <td>{nextma}<br></br>{nextma2}<br></br><h3>VISTA PARADISO</h3></td>
+  </tr></tbody></table>
+</aside>
 
 </div>);
 }
--- src/component/setting.js
+++ src/component/setting.js
@@ -1,17 +1,48 @@
 import $ from 'jquery'
 import React from 'react';
+import { NgToggleModule } from 'ng-toggle-button';
 
 export default function Setting(){
 $(document).ready(function(){
-$(document).on('click','#setting',function(){$('section').toggle()})
-$(document).on('click','#next',function(){$('#nextdiv').toggle()})
+$(document).on('click','#setting',function(){$('#settingdiv').toggle()})
+$(document).on('click','#next',function(){
+  $('#timetable').toggle()
+  $('#nextdiv').toggle()})
+
+
 })
-return (<section>
-<div className='twotoggles'> Visibility 
-      <div>Public</div> 
-      <div>Private</div>
-  </div>
 
+return (<div id='settingdiv'>
+<button>test</button>
+<ng-toggle
+  [value]="1"
+  [disabled]="false"
+  [width]="100"
+  [height]="50"
+  [margin]="0"
+  [fontSize]="100"
+  [labels]="{
+  "unchecked": "",
+  "checked": ""
+}"
+  [color]="{
+  "unchecked": "#960000",
+  "checked": "#006464"
+}"
+  [switchColor]="{
+  "checked": "#00a388",
+  "unchecked": "red"
+}"
+  [values]="{
+  "unchecked": 0,
+  "checked": 1
+}"
+  [fontColor]="{
+  "checked": "#00a388",
+  "unchecked": "#f45a32"
+}">
+</ng-toggle>
+      
 
-</section>);
+</div>);
 }
